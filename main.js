b25c463d07c6bd46397f27c2ac31ab397b1c2e2c
Murodjon1407 <murodjonshamshumetov@gmail.com>
2025-01-14 15:57:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// const ovoz = prompt("hayvon nomi")
+
+// function bot (a){
+//     if(a == "it"){
+//         alert("woowoooow")
+//     }else if(a == "mushuk"){
+//         alert("miyauv")
+//     }else if (a == ""){
+//         alert("hayvon nomini kiriting")
+//     }else{
+//         alert("ushbu hayvon nomi mavjud emas")
+//     }
+// }
+// bot(ovoz)
+
